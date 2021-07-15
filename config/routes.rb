dc2152efe2d95d860995af3ebbe812d597615ea5
Ваше Имя <martinushka536@yandex.ru>
2021-07-15 09:49:39 +0300

    step 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/new', to: 'posts#new'
+  get 'posts/:id', to: 'posts#show', contstraint: { id: /\d+/}
+  get 'posts/:topic', to: 'posts#topic'
+
+
+  post 'posts/create', to: 'posts#create'
+  get 'posts/edit/:id', to: 'posts#edit'
+  post 'posts/update/:id', to: 'posts#update'
+
   root to: "posts#index"
-  get 'posts/:id', to: 'posts#show'
 end
